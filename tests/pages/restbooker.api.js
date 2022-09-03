bb226d57a94b523156d01a76e6fff535cee765e7
Yahya H <yahyhfidz@outlook.com>
2022-09-04 00:39:32 +0700

    test

--- tests/pages/restbooker.api.js
+++ tests/pages/restbooker.api.js
@@ -1,7 +1,10 @@
-import BaseAPI from '../pages/restbooker.api.js';
+import BaseAPI from '../pages/base.api.js';
 
 const RestbookerAPI = {
     createToken: (data) => BaseAPI.post('/auth', data),
+    getBookingIds: (data) => BaseAPI.get('/booking', data),
+    getBookingbyID:(data) => BaseAPI.get('/booking/:id',data),
+
     
 }
 
