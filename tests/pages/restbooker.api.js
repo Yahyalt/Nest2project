26d1b68af0a334d8f407d7f3833acadc5b4ee001
rifqi23ahmad <rifqi23ahmad@gmail.com>
2022-09-04 23:27:34 +0700

    on progress update

--- tests/pages/restbooker.api.js
+++ tests/pages/restbooker.api.js
@@ -2,7 +2,7 @@ import BaseAPI from '../pages/base.api.js';
 
 const RestbookerAPI = {
     createToken: (data) => BaseAPI.post('/auth', data),
-    
+    updateput: (data) => BaseAPI.put('/booking/3', data),
 }
 
 export default RestbookerAPI ;
