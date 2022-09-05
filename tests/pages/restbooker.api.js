5080e8d18c06802444749f6be4f2904f6beca01d
Yahya H <yahyhfidz@outlook.com>
2022-09-05 22:12:55 +0700

    push get allbooking ID

--- tests/pages/restbooker.api.js
+++ tests/pages/restbooker.api.js
@@ -4,7 +4,8 @@ const RestbookerAPI = {
     createToken: (data) => BaseAPI.post('/auth', data),
     updateput: (data) => BaseAPI.put('/booking/3', data),
     healthCheck: (data) => BaseAPI.get('/ping', data),
-    getToken: (data) => BaseAPI.post('/auth', data)
+    getToken: (data) => BaseAPI.post('/auth', data),
+    getAllbookingIds: (data) => BaseAPI.get('/booking', data)
 
 
 }
--- tests/scenarios/getAllBookingID.test.js
+++ tests/scenarios/getAllBookingID.test.js
@@ -4,9 +4,11 @@ import RestbookerAPI from "../pages/restbooker.api";
 
 describe('Get all booking IDs', () =>{
 it('should successfully see list of all booking IDs', async () => {
-    const response = await RestbookerAPI.getBookingIds();
+    const response = await RestbookerAPI.getAllbookingIds();
 
     assert.equal(response.status, 200);
+    assert.containsAllKeys(response, ["data"]);
+    //assert.isArray(response.data.data);
     
 
 });   
