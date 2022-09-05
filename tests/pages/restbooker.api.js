5fdbb1ccaaf502ac76bbf61ae6cfc38d4c25ef83
ardimuldian <ardimuldian@gmail.com>
2022-09-05 22:43:41 +0700

    change restbooker.api

--- tests/pages/restbooker.api.js
+++ tests/pages/restbooker.api.js
@@ -4,9 +4,7 @@ const RestbookerAPI = {
     createToken: (data) => BaseAPI.post('/auth', data),
     updateput: (data) => BaseAPI.put('/booking/3', data),
     healthCheck: (data) => BaseAPI.get('/ping', data),
-    getToken: (data) => BaseAPI.post('/auth', data)
+    getToken: (data) => BaseAPI.post('/auth', data),
 
 
-}
-
-export default RestbookerAPI;
+};
