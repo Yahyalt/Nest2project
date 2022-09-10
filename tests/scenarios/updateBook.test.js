b7fc5ca8aea5aeb7a16c88ad23372f4aea3c4072
rifqi23ahmad <rifqi23ahmad@gmail.com>
2022-09-10 21:16:29 +0700

    Update DONE

--- tests/scenarios/updateBook.test.js
+++ tests/scenarios/updateBook.test.js
@@ -2,17 +2,21 @@ import { assert } from 'chai';
 import RestbookerAPI from '../pages/restbooker.api.js';
 import * as data from '../data/updatedbooking.js';
 import BaseAPI from '../pages/base.api'
+import request from 'supertest';
 
-describe('As a guest, I want to update user data (using PUT method)', () => {
+
+describe.only('As a guest, I want to update user data (using PUT method)', () => {
     it('Should be succesfully update user data', async () => {
+    
+ 
         const response = await RestbookerAPI.updatePut(data.UPDATE_PUT);
-        request (BaseAPI)
-            .put('/booking/5' )
+        request(BaseAPI)
+            .put('/booking/7' )
             .send(data)
             .set('Accept', 'application/json')
             .set('Content-Type', 'application/json')
             .set('Cookie', 'token=' )
-            .end(function(err,) {
+            .end(function(err, response) {
                 assert(response.data.firstname).to.be.equal(updatedbooking.firstname);
                 assert(response.data.lastname).to.be.equal(updatedbooking.lastname);
                 assert(response.data.totalprice).to.be.equal(updatedbooking.totalprice);
