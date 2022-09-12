39ea29df6306a4704e3be32bc2be167fc761f402
ardimuldian <ardimuldian@gmail.com>
2022-09-12 19:19:46 +0700

    "change data"

--- tests/data/user.data.js
+++ tests/data/user.data.js
@@ -29,17 +29,7 @@ export const CREATE_TOKEN_F = {
         "password": "password123"
 }
 
-export const CREATE_BOOKING_ID = {
-    "firstname" : "Jim",
-    "lastname" : "Brown",
-    "totalprice" : 111,
-    "depositpaid" : true,
-    "bookingdates" : {
-        "checkin" : "2018-01-01",
-        "checkout" : "2019-01-01"
-    },
-    "additionalneeds" : "Breakfast"
-}
+
 
 
 
