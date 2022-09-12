8b919daaefce2126c1342063ef99fe641afb6f49
marthawege <marthagultom02@gmail.com>
2022-09-12 22:26:53 +0700

    remove comment

--- tests/scenarios/getBookingbyID.test.js
+++ tests/scenarios/getBookingbyID.test.js
@@ -2,12 +2,11 @@ import { assert } from "chai";
 import RestbookerAPI from "../pages/restbooker.api.js";
 
 describe('As a User, I want list of user', () =>{
-    it.only('should successfully see list of user', async () => {
+    it('should successfully see list of user', async () => {
         const response = await RestbookerAPI.getBookingbyID();
     
         assert.equal(response.status, 200);
         assert.containsAllKeys(response.data, ["additionalneeds", "bookingdates", "depositpaid", "firstname", "lastname", "totalprice"]);
-        
         
         assert.isString(response.data.firstname);
         assert.isString(response.data.lastname);
@@ -17,7 +16,6 @@ describe('As a User, I want list of user', () =>{
         assert.toString(response.data.checkin);
         assert.toString(response.data.checkout);
         assert.isString(response.data.additionalneeds);
-
 
     
     });
