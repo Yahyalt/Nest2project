38c089e7ffa8d1e7ed1ccc79f44b7534fa2f0197
rifqi23ahmad <rifqi23ahmad@gmail.com>
2022-09-12 19:24:55 +0700

    Create Booking Done

--- tests/scenarios/createBookingID.test.js
+++ tests/scenarios/createBookingID.test.js
@@ -1,24 +1,32 @@
 import { assert } from 'chai';
 import RestbookerAPI from '../pages/restbooker.api';
 import * as data from '../data/user.data';
-import BASEAPI from '../pages/base.api';
+import BaseAPI from '../pages/base.api';
 import request from 'supertest';
 
-describe.only('Verify User Can Create Field', () => {
-    it('Verify User Can Add Booking Data', async () => {
-        BASEAPI.post('/auth')
+describe('Verify user can create booking', () => {
+    it('Should be succesfully create booking id', async () => {
         const response = await RestbookerAPI.createBookingids(data.CREATE_BOOKING_ID);
-        //chai utk assertion
-        request (BASEAPI)
-    .post('/booking')
-    assert(response.statusCode).to.be.equal(200);
-    assert(response.data.firstname).to.be.equal(updatedbooking.firstname);
-    assert(response.data.lastname).to.be.equal(updatedbooking.lastname);
-    assert(response.data.totalprice).to.be.equal(updatedbooking.totalprice);
-    assert(response.data.depositpaid).to.be.equal(updatedbooking.depositpaid);
-    assert(response.data.bookingdates.checkin).to.be.equal(updatedbooking.bookingdates.checkin);
-    assert(response.data.bookingdates.checkout).to.be.equal(updatedbooking.bookingdates.checkout);
-    assert(response.data.additionalneeds).to.be.equal(updatedbooking.additionalneeds);
+        request(BaseAPI)
+            .post('/booking' )
+            .send(data.CREATE_BOOKING_ID)
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .end(function(err, response) {
+                assert(res.statusCode).to.be.equal(200);
+                assert(response.data.firstname).to.be.equal(updatedbooking.firstname);
+                assert(response.data.lastname).to.be.equal(updatedbooking.lastname);
+                assert(response.data.totalprice).to.be.equal(updatedbooking.totalprice);
+                assert(response.data.depositpaid).to.be.equal(updatedbooking.depositpaid);
+                assert(response.data.bookingdates.checkin).to.be.equal(updatedbooking.bookingdates.checkin);
+                assert(response.data.bookingdates.checkout).to.be.equal(updatedbooking.bookingdates.checkout);
+                assert(response.data.additionalneeds).to.be.equal(updatedbooking.additionalneeds);
+                if (err) {
+                    throw err;
+                };
+                
+            });
+    });
 
-    })
+   
 });
